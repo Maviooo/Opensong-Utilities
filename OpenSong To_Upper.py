64cc73a7d05c506d795be0a2dd95c85f9f300351
Mavio <marolojek@gmail.com>
2022-03-18 23:05:02 +0100

    Initial

---
+++ OpenSong To_Upper.py
@@ -0,0 +1,34 @@
+import os
+import re
+in_path = input("Specify directory where files are located: ")
+content = ""
+for inFile in os.listdir(in_path):
+    try:
+        global_path = in_path + "\\" + inFile
+        print(global_path)
+
+        f = open(global_path,mode= "r" ,encoding="utf8")
+        content = f.read()
+        f.close()
+
+    except:
+        print("IO Error!!!")
+
+    try:
+        sep = "[V1]"
+        lyrics_1 = content.split(sep, 1)
+        sep_2 = "<"
+        lyrics_2 = lyrics_1[1].split(sep_2, 1)
+        fin = content.replace(lyrics_2[0], lyrics_2[0].upper())
+
+    except:
+        continue
+    try:
+        f= open(global_path,mode="w",encoding="utf8")
+        f.write(fin)
+        f.close()
+    
+    except:
+        print("IO Error!!!")
+print("Done!")
+
--- OpenSong_Delete_tags.py
+++ OpenSong_Delete_tags.py
@@ -1,29 +1,38 @@
 import os
 import re
 in_path = input("Specify directory where files are located: ")
-content = ""
 for inFile in os.listdir(in_path):
     try:
         global_path = in_path + "\\" + inFile
         print(global_path)
 
+        content = ""
         f = open(global_path,mode= "r" ,encoding="utf8")
         content = f.read()
         f.close()
+    except:
+        print("Error opening the file")
+
+    try:
+        content_striped = ""
+        content_striped2 = ""
+        content_striped = re.sub(r'[\s\S]*(<lyrics>)','',content)
+        content_striped2 = re.sub(r'(</lyrics>)(.|\n)*','',content_striped, flags=re.DOTALL)
+        content_striped2 = content_striped2.replace("||", "")
+        content_striped2 = content_striped2.replace("|", "")
+        content_striped2 = re.sub(r'\..*\n',"",content_striped2)
+        content_striped2 = content_striped2.replace(":/ ", "")
+        content_striped2 = content_striped2.replace(":/", "")
+        content_striped2 = content_striped2.replace("/:", "")
 
     except:
-        print("IO Error!!!")
-    
-    content_striped = re.sub(r'[\s\S]*(<lyrics)[\s\S]','',content)
-    
-    content_striped2 = re.sub(r'(</lyrics>).*','',content_striped, flags=re.DOTALL)
+        print("Error processing file")
 
     try:
         f= open(global_path,mode="w",encoding="utf8")
         f.write(content_striped2)
         f.close()
-    
     except:
-        print("IO Error!!!")
+        print("Error closing the file")
 print("Done!")
 
